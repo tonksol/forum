8d2bdfa444132b25a08cfa47b038547c7543e28a
Tonke Bult <tonksol@gmail.com>
2018-04-17 10:37:14 +0200

    clear the glutter

--- business/userProfile.php
+++ business/userProfile.php
@@ -11,7 +11,6 @@
         $email = $_POST["email"];
         $username = $_POST["username"];
         $quote = $_POST["quote"];
-        $badge = $row["badge"];
         // UPDATE user SET firstName = '$firstname', prefix = '$prefix', lastName = '$lastname', birthday = '$birthday', email = '$email', userName = '$username', quote = '$quote'
         // WHERE userID = $userID;
         $query = "CALL proc_update_userinfo('$userID','$firstname', '$prefix', '$lastname', '$birthday', '$email', '$username', '$quote')";
@@ -24,29 +23,14 @@
         }
     }
   
-    
-    // update sql + id 
-    // nieuwe data valideren 
     $userID = $_SESSION['user_id'];
-    // to do : fix the userbadge for the userprofile
-    // 2 queries combineren tot 1 d.m.v. joins 
 
-     $query = "SELECT * FROM badge as b
-     JOIN userBadge as ub ON b.badgeID = ub.badgeID
-     WHERE ub.userID = $userID";
-
-     $query = "SELECT * FROM user WHERE userID = $userID";
+    $query = "SELECT * FROM user WHERE userID = $userID";
      
 // "CALL proc_insert_new_user('tonksol@mail.com')";                  
     $result = mysqli_query($connection, $query);
     if ($result->num_rows > 0){
-        // output data of each row. Using an multidimensional array to get all the badges
-        // $array1 = array();
-        // $i = 1;
-
         while ($row = $result->fetch_assoc()) {
-            // $i++;
-
             $userId = $row["userID"];
             $firstname = $row["firstName"];
             $prefix = $row["prefix"];
@@ -56,24 +40,17 @@
             $email = $row["email"];
             $username = $row["userName"];
             $quote = $row["quote"];
-            // to do badge in multidimensioal array 
-            // $badge = $array1[$row['badgeImage']];
+
+            // SELECT * FROM badge as b JOIN userBadge as ub ON b.badgeID = ub.badgeID WHERE ub.userID = $userID
             $query2 = "SELECT * FROM badge as b
             JOIN userBadge as ub ON b.badgeID = ub.badgeID
             WHERE ub.userID = $userID";
-            // echo $query2;
+
             $result2 = mysqli_query($connection, $query2);
             while ($row = $result2->fetch_assoc()) {
                     $badge[] = $row['badgeImage'];
-                    // var_dump($badge);
             }
         }
-        // $userdata = $result->fetch_assoc();
-        // $userid = $userdata['userID'];
-
-
-            // $badge[$i]['badgeImage'] = $row["badgeImage"];
-
         
     } 
     
